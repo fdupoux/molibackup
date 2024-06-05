ba8be7b4f2b7a878d044265184b592e2cf9f8a27
Francois Dupoux <fdupoux@users.noreply.github.com>
2024-06-05 06:10:38 +0100

    Update golang to 1.22.4

--- docker.sh
+++ docker.sh
@@ -8,7 +8,7 @@ echo "curdir=${curdir}"
 echo "repodir=${repodir}"
 
 # Docker options
-dockerimg="golang:1.21.6@sha256:efe985ec6ca642b035ea2896c7e975f0ec04063bcdb206bbff0d4e1c74ba3ff7"
+dockerimg="golang:1.22.4@sha256:969349b8121a56d51c74f4c273ab974c15b3a8ae246a5cffc1df7d28b66cf978"
 dockeropt="--rm -it --volume=${repodir}:/home --workdir=/home"
 
 # Reuse existing GOPATH directory if it exists
